1a5433f9dbac9dc8ea8982da1d9c8400e74030e6
Kobe <kobetbuckley@gmail.com>
2023-06-30 23:15:10 -1000

    in progrress

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 alert("active")
 const animeId = 1; // Replace with the desired anime ID
 
-fetch(`https://api.jikan.moe/v4/anime/${animeId}/full`)
+fetch(`https://api.jikan.moe/v4/anime/${1}/full`)
   .then(response => response.json())
   .then(data => {
     // Process the retrieved data
