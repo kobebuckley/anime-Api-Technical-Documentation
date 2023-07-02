edc7e2d830d0edec9374871930eac1ce2a92f754
Kobe <kobetbuckley@gmail.com>
2023-07-01 14:46:04 -1000

    success; added demon slayer data through api call. This will help to go over coding example for the technical documentation website that is used for api calls. Making this jikan api a quick guide as the project, but using an anime theme for examples

--- scripts.js
+++ scripts.js
@@ -1,13 +1,30 @@
-alert("active")
-const animeId = 1; // Replace with the desired anime ID
+const animeId = 38000; // Demon Slayer: Kimetsu no Yaiba
 
-fetch(`https://api.jikan.moe/v4/anime/${1}/full`)
-  .then(response => response.json())
-  .then(data => {
-    // Process the retrieved data
-    console.log(data);
-  })
-  .catch(error => {
-    // Handle any errors
+let jsonData; // Global variable to store the JSON data
+
+async function fetchData() {
+  try {
+    const response = await fetch(`https://api.jikan.moe/v4/anime/${animeId}/full`);
+    const retrievedData = await response.json();
+    console.log(retrievedData);
+    jsonData = retrievedData.data;
+    console.log("here are the available data options: ", jsonData)
+    grabUrl();
+
+
+
+  } catch (error) {
     console.error(error);
-  });
+  }
+}
+
+
+function grabUrl() {
+  console.log("url data here :",jsonData['url']);
+}
+
+
+
+
+
+fetchData();
